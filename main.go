bb1901b4037c6c70d8c537e58662ed464b4a2e88
Laurent Lévêque <lleveque.ext@gmail.com>
2017-11-02 00:20:12 +0100

    Make user pass its package import path as parameter

--- main.go
+++ main.go
@@ -15,24 +15,22 @@
 package main
 
 import (
+    "flag"
     "log"
     "os"
 )
 
-var outputFolder = "python"
+// var outputFolder = "python"
 
-func processFile(inputPath string) (err error) {
-    packageName, functions := loadFile(inputPath)
-
-    // TODO Construct full package name
-    fullPackageName := "github.com/lleveque/greeting"
+func processFile(packagePath string, filePath string, outputFolder string) (err error) {
+    packageName, functions := loadFile(filePath)
 
     // Create subfolder
     if err = os.MkdirAll(outputFolder, os.ModePerm); err != nil {
         return
     }
 
-    outputPath, err := getRenderedPath(inputPath, outputFolder, "go")
+    outputPath, err := getRenderedPath(filePath, outputFolder, "go")
     if err != nil {
         return
     }
@@ -44,11 +42,11 @@ func processFile(inputPath string) (err error) {
     }
     defer output.Close()
 
-    if err = render(output, fullPackageName, packageName, functions); err != nil {
+    if err = render(output, packagePath, packageName, functions); err != nil {
         return
     }
 
-    outputPathC, err := getRenderedPath(inputPath, outputFolder, "c")
+    outputPathC, err := getRenderedPath(filePath, outputFolder, "c")
     if err != nil {
         return
     }
@@ -60,7 +58,7 @@ func processFile(inputPath string) (err error) {
     }
     defer outputC.Close()
 
-    if err = renderC(outputC, fullPackageName, packageName, functions); err != nil {
+    if err = renderC(outputC, packagePath, packageName, functions); err != nil {
         return
     }
     return
@@ -70,14 +68,16 @@ func main() {
     log.SetFlags(0)
     log.SetPrefix("goprotopy: ")
 
-    if len(os.Args) != 2 {
-        log.Fatalf("Usage : goprotopy filename.go")
-        return
-    }
+    // packagePath := "github.com/lleveque/greeting"
 
-    for _, path := range os.Args[1:] {
-        if err := processFile(path); err != nil {
-            log.Fatalf("Error processing file %s: %s", path, err)
+    packagePath := flag.String("packagePath", "", "the full path of the go package you want to generate Python bindings for")
+    outputFolder := flag.String("outputFolder", "python", "the output folder for generated files")
+
+    flag.Parse()
+
+    for _, filePath := range flag.Args() {
+        if err := processFile(*packagePath, filePath, *outputFolder); err != nil {
+            log.Fatalf("Error processing file %s: %s", filePath, err)
         }
     }
 }
